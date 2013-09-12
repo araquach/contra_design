09062214501f08a1bbbacbf8620105ecb457a27a
Araquach <adam@contra-design.com>
2013-09-12 20:02:16 +0100

    styled the slider

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,19 +7,24 @@ $this->pageTitle=Yii::app()->name;
 	Yii::app()->clientScript->registerCoreScript('jquery');
 	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/scripts/unslider.min.js');
 	Yii::app()->clientScript->registerScript('unslider', '$(function() {
-	    $(".banner").unslider();
+	    $(".banner").unslider({
+	    	speed: 500,               
+	    	delay: 6000,               
+	    	keys: true,               
+	    	dots: true,               
+	    	fluid: true,              
+	    });
 	});');
-
 ?>
 
 <div id="home">
 
 <div class="banner">
     <ul>
-        <li>This is a slide.</li>
-        <li>This is another slide.</li>
-        <li>This is a final slide.</li>
+        <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/main/home/doodle-communication.jpg" alt="" /></li>
+        <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/main/home/man-using-a-computer.jpg" alt="" /></li>
+        <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/main/home/social-media.jpg" alt="" /></li>
     </ul>
-</div>
+</div> <!--.banner-->
 
 </div> <!--#home-->
